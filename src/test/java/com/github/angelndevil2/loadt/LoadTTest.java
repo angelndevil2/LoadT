1e2c1be01c88aa3f22212e674233c75185affc22
k <jhkim@aimtog.co.kr>
2016-02-18 09:39:31 +0900

    read jetty server port from properties for test

--- src/test/java/com/github/angelndevil2/loadt/LoadTTest.java
+++ src/test/java/com/github/angelndevil2/loadt/LoadTTest.java
@@ -5,27 +5,47 @@ import com.github.angelndevil2.loadt.common.JMeterCalculator;
 import com.github.angelndevil2.loadt.common.LoadTException;
 import com.github.angelndevil2.loadt.common.SystemInfoCollector;
 import com.github.angelndevil2.loadt.jetty.JettyServer;
+import com.github.angelndevil2.loadt.jetty.PropList;
 import com.github.angelndevil2.loadt.listener.CSVFileSaver;
 import com.github.angelndevil2.loadt.listener.ConsoleResultViewer;
 import com.github.angelndevil2.loadt.listener.ConsoleStatisticViewer;
 import com.github.angelndevil2.loadt.listener.IResultSaver;
 import com.github.angelndevil2.loadt.loadmanager.LoadManagerType;
 import com.github.angelndevil2.loadt.util.PropertiesUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author k, Created on 16. 2. 6.
  */
+@Slf4j
 public class LoadTTest {
+
+    static final Properties jettyProp;
+    static {
+        try {
+            PropertiesUtil.setDirs("src/dist");
+        } catch (IOException e) {
+            log.error("base directory setting error.", e);
+        }
+
+        jettyProp = new Properties();
+
+        try {
+            jettyProp.load(new FileInputStream(PropertiesUtil.getJettyPropertiesFile()));
+        } catch (IOException e) {
+            log.error("error loading jetty properties.", e);
+        }
+    }
 
     @Test
     public void testLoadT() throws LoadTException, IOException, InterruptedException {
 
         LoadT loadT = new LoadT();
-
-        PropertiesUtil.setDirs("src/dist");
 
         Thread jetty = new Thread(new JettyServer());
         jetty.setDaemon(true);
@@ -51,7 +71,7 @@ public class LoadTTest {
         loadT.setNumberOfThread(name, 1);
 
        // add http sampler
-        loadT.addHttpSampler(name, "websphere", "localhost", 1080, "/", HTTPMethod.GET, "localhost");
+        loadT.addHttpSampler(name, "websphere", "localhost", Integer.valueOf((String)jettyProp.get(PropList.HTTP_PORT)), "/", HTTPMethod.GET, "localhost");
 
         // add system information collector with domain "192.168.100.241"
         SystemInfoCollector systemInfoCollector = new SystemInfoCollector("localhost");
